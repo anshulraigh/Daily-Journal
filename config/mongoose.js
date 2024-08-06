0082905029e38be7da68e8beca13c9d809dbaa49
Anshul Rai <anshulrai02092003@gmail.com>
2024-08-06 18:11:06 +0530

    up - to rm dep methods

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,13 +1,11 @@
 const mongoose = require("mongoose");
-require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log("Connected to MongoDB");
-}).catch((err) => {
-  console.error("Error connecting to MongoDB", err);
-});
+mongoose.connect(process.env.MONGO_URL, {})
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB", err);
+  });
 
 module.exports = mongoose;
